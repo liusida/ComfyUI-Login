c029856539926067e05ee4ca8b0de6763201dc0f
Sida Liu <learner.sida.liu@gmail.com>
2024-06-07 16:03:38 +0800

    free memory if idle for 30mins

--- js/freeMemory.js
+++ js/freeMemory.js
@@ -8,8 +8,28 @@ import { app } from "/scripts/app.js";
 app.registerExtension({
   name: "Comfy.Login.FreeMemory",
   async init(app) {
-    window.addEventListener('beforeunload', () => {
+    let idleTimer;
+
+    // Function to send the beacon
+    function sendFreeMemoryRequest() {
       navigator.sendBeacon("/free", JSON.stringify({ unload_models: true, free_memory: true }));
-    });
+    }
+
+    // Reset the idle timer
+    function resetIdleTimer() {
+      clearTimeout(idleTimer);
+      idleTimer = setTimeout(sendFreeMemoryRequest, 1800000); // 1800000 ms = 30 mins
+    }
+
+    // Listen for any of these events to reset the timer
+    window.addEventListener('mousemove', resetIdleTimer, false);
+    window.addEventListener('keydown', resetIdleTimer, false);
+    window.addEventListener('scroll', resetIdleTimer, false);
+
+    // Setup the initial timer
+    resetIdleTimer();
+
+    // Send request when the window is about to be closed
+    window.addEventListener('beforeunload', sendFreeMemoryRequest);
   },
 });
